c22454f8ca6f38b33d171dbbe8c922cec3d1bf0e
OkvyanRizkyPramarta <pramarta26@gmail.com>
2022-10-19 13:35:41 +0700

    Mengubah style dari halaman home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -70,7 +70,7 @@
                 <div class="col-lg-6">
                     <h1 class="display-4 text-uppercase mb-4" style="color:white;">Blog</h1>
                     <h5 class="text-uppercase text-primary mb-3">Clita elitr et amet et ipsum sea. Ipsum stet kasd ea et no est duo diam. Lorem dolores eos ut nonumy ipsum sit clita lorem no amet dolor dolore, stet sit dolor justo</h5>
-                    <p class="mb-4">Eirmod est dolor nonumy sea amet dolore erat sit dolor et dolor vero. Tempor ipsum at justo amet at ipsum justo. Aiam kasd sea sit dolor duo elitr dolor amet, justo est ipsum amet dolor ut ipsum. Labore diam et nonumy amet dolores. Volup sit labore dolores erat, magna justo sed lorem kasd ea dolor. Labore sit clita invidunt, est dolores lorem sed ipsum kasd no amet ipsum.</p>
+                    <p class="mb-4"  style="color:white;">Eirmod est dolor nonumy sea amet dolore erat sit dolor et dolor vero. Tempor ipsum at justo amet at ipsum justo. Aiam kasd sea sit dolor duo elitr dolor amet, justo est ipsum amet dolor ut ipsum. Labore diam et nonumy amet dolores. Volup sit labore dolores erat, magna justo sed lorem kasd ea dolor. Labore sit clita invidunt, est dolores lorem sed ipsum kasd no amet ipsum.</p>
                     <a href="" class="btn btn-primary text-uppercase py-3 px-5">Read More</a>
                 </div>
             </div>
