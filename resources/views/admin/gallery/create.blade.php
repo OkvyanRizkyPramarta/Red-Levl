e3132bafbe90d2516b066a0ee93b7d943317d950
OkvyanRizkyPramarta <pramarta26@gmail.com>
2022-10-25 19:12:34 +0700

    Form Insert Dan Data Table Gallery

---
+++ resources/views/admin/gallery/create.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.admin.master')
+
+@section('content')
+<!-- Breadcomb area Start-->
+    <div class="breadcomb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="breadcomb-list">
+						<div class="row">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+								<div class="breadcomb-wp">
+									<div class="breadcomb-icon">
+										<i class="notika-icon notika-form"></i>
+									</div>
+									<div class="breadcomb-ctn">
+										<h2 style="color:white;">Form Create Gallery</h2>
+										<p style="color:white;">Please fill in the required fields</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Breadcomb area End-->
+    <!-- Form Element area Start-->
+    <div class="form-element-area">
+        <div class="container">
+        <form method="POST" action="#" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="form-element-list mg-t-30">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12">
+                                <div class="form-group ic-cmp-int float-lb floating-lb">
+                                    <div class="form-ic-cmp">
+                                    </div>
+                                    <div class="nk-int-st">
+                                        <label>Name</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="form-group ic-cmp-int float-lb floating-lb">
+                                    <div class="form-ic-cmp">
+                                    </div>
+                                    <div class="nk-int-st">
+                                        <label>Picture Gallery</label>
+                                        <input type="file" class="form-control" required name="image">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="form-group ic-cmp-int float-lb floating-lb">
+                                    <div class="form-ic-cmp">
+                                    </div>
+                                    <div class="nk-int-st">
+                                        <label>Video Gallery</label>
+                                        <input type="file" class="form-control" name="video">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><br>
+            <button class="btn btn-default btn-icon-notika col-md-2">
+                <i class="notika-icon notika-left-arrow"></i> 
+                BACK
+            </button>
+            <button class="btn btn-default btn-icon-notika col-md-2" style="float: right;">                
+                SEND
+                <i class="notika-icon notika-right-arrow"></i> 
+            </button>
+        </form>
+        </div>
+    </div>
+    <!-- Form Element area End-->
+@endsection
